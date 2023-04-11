057fc83c6afd1fde9438434f1ce1772c64188ac7
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-04-11 00:53:53 -0300

    handled all errors in summary method. TODO collect info in dictionary

--- pipeline/manage_dataset.py
+++ pipeline/manage_dataset.py
@@ -86,11 +86,29 @@ class DatasetManager:
         for file in files:
             name = re.split(r'/|\\', file)[-1]
             name = name.split('.')[0] + '.txt'
-            if not os.path.exists(self.txt + name):
+            name = self.txt + name
+            if not os.path.exists(name):
                 raise EnvironmentError(name + ' does not exists')
-            
-
-
+            with open(name, mode='r', encoding='utf8') as f:
+                for i, line in enumerate(f):
+                    args = line.split(' ')
+                    n = len(args)
+                    if n < 5:
+                        error = name + ' has one of the lines with '
+                        error += 'less than the spected values'
+                        raise EnvironmentError(error)
+                    for j, arg in enumerate(args):
+                        if j == 0:
+                            if not arg.isnumeric():
+                                error = 'line '.format(i) + 'column'.format(j) + ' from '
+                                error += name + ' has to be an integer as class name'
+                                raise EnvironmentError(error)
+                        else:
+                            if not arg.replace('.', '', 1).isnumeric():
+                                error = 'line '.format(i) + 'column'.format(j) + ' from '
+                                error += name + '\nhas to be an float representing '
+                                error += 'one of the coordinates of the bounding box'
+                                raise EnvironmentError(error)
 if __name__ == '__main__':
     loc = ['./images/', './images/']
     new_loc = ['./train/images/', './train/annotations/']
@@ -98,3 +116,52 @@ if __name__ == '__main__':
     dataset = DatasetManager()
     dataset.change_folders(loc=loc, new_loc=new_loc, extensions=extensions)
     print(dataset.__dict__)
+
+def fix_label(start: int, end: int, a: int, b: int, path: str='./images/images/'):
+    """
+    swaps labels in given directory. is created to solve the a labelimg bug
+    @start and @end: the indices of the slice to fix being @end exlusive
+    @a and @b: the variables to swap
+    @path: the directory containing the annotations
+    """
+    if not isinstance(start, int):
+        raise TypeError('start must be an integer')
+    if not isinstance(end, int):
+        raise TypeError('end must be an integer')
+    if not isinstance(a, int):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, int):
+        raise TypeError('b must be an integer')
+    if not isinstance(path, str):
+        raise TypeError('path must be a string')
+    path = './' + path if path[0] != '.' else path
+    if not os.path.exists(path=path):
+        raise EnvironmentError(path + ' does not exist')
+    path += '/*.txt' if path[-1] != '/' else '*.txt'
+
+    def custom(x):
+        """
+        function to sort first numerically descending
+        and then sort alphabetically ascending
+        """
+        x = re.split(r'/|\\', x)[-1]
+        #return x
+        a = int(re.search(r'\d+', x).group())
+        b = ''.join(re.findall(r'\D+', x))
+        return a, b
+
+    names = sorted(glob(pathname=path), key=custom)[start: end]
+    for i, name in enumerate(names):
+        with open(name, mode='r', encoding='utf8') as f:
+            new = ""
+            for line in f:
+                line = line.split(' ')
+                line[0] = str(a) if str(line[0]) == str(b) else str(b)
+                new += " ".join(line) + '\n'
+            if i == 0:
+                print(re.search(r'\d+', re.split(r'/|\\', name)[-1]).group())
+                print(re.findall(r'\D+', re.split(r'/|\\', name)[-1]))
+                print(i, name, '\n', new)
+
+#if __name__ == '__main__':
+#    fix_label(start=0, end=-1, a=15, b=16, path='./images/images/')
