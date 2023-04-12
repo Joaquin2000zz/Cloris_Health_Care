e3734ceb74f2edbc3ccd18cd4456fe32549aad19
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-04-12 14:39:46 -0300

    ended split_data method and updated summary method for work also with splitted data

--- pipeline/manage_dataset.py
+++ pipeline/manage_dataset.py
@@ -3,7 +3,6 @@
 module which contains DatasetManager class
 """
 from glob import glob
-import numpy as np
 import os
 import random
 import re
@@ -15,7 +14,8 @@ class DatasetManager:
     class which perfors several methods to manage locations from files
     and visualize the labels quantities
     """
-    def __init__(self, jpg: str=None, txt: str=None):
+
+    def __init__(self, jpg: str = None, txt: str = None):
         """
         class constructor
         @jpg: must contain the path of the images
@@ -24,15 +24,18 @@ class DatasetManager:
         if (jpg and not isinstance(jpg, str)) and (txt and not isinstance(txt, str)):
             error = 'check the jpg and txt types. they must be strings)'
             error += '\nrepresenting the paths of the images '
-            error +=  'and annotations folders respectively'
+            error += 'and annotations folders respectively'
             raise TypeError(error)
         self.jpg = jpg
         self.txt = txt
         self.is_splitted = False
+        self.train = None
+        self.val = None
+        self.test = None
 
-    def change_folders(self, loc: list=[],
-                       new_loc: list=[], extensions: list=[],
-                       n=None, verbose: float=True) -> None:
+    def change_folders(self, loc: list = [],
+                       new_loc: list = [], extensions: list = [],
+                       n=None, verbose: float = True) -> None:
         """
         method which changes directories of given extensions recursively
         """
@@ -79,60 +82,141 @@ class DatasetManager:
         """
         makes as summary of the dataset. By computing the mean of each label,
         counts the dominant label in each image, compute the mean,
-        the max value for each label and saves everything in the info attribute.
+        the variance, the standard deviation, the max value for each label
+        and saves everything in the info attribute.
         also checks if each image of the dataset has its corresponding annotation
         and if the annotations are not corrupted
+        if self.is_splitted == True computes the sample variance for each sample
         returns the summary attribute
         """
-        if not self.jpg or not self.txt:
-            raise SyntaxError('check the jpg and txt attributes')
-        self.jpg += '/' if self.jpg[-1] != '/' else ''
-        self.txt += '/' if self.jpg[-1] != '/' else ''
-        files = glob(self.jpg + '*.jpg')
-        if not files:
-            raise EnvironmentError(self.jpg + ' has no .jpg files')
-        self.info = {}
-        for file in files:
-            name = re.split(r'/|\\', file)[-1]
-            name = name.split('.')[0] + '.txt'
-            name = self.txt + name
-            if not os.path.exists(name):
-                raise EnvironmentError(name + ' does not exists')
-            x = {}
-            with open(name, mode='r', encoding='utf8') as f:
-                for i, line in enumerate(f):
-                    args = line.split(' ')
-                    n = len(args)
-                    if n < 5:
-                        error = name + ' has one of the lines with '
-                        error += 'less than the spected values'
-                        raise EnvironmentError(error)
-                    for j, arg in enumerate(args):
-                        if j == 0:
-                            if not arg.isnumeric():
-                                error = 'line '.format(i) + 'column'.format(j) + ' from '
-                                error += name + ' has to be an integer as class name'
-                                raise EnvironmentError(error)
-                            if not x.get(arg):
-                                x[arg] = 1
+        if not self.is_splitted:
+            if not self.jpg or not self.txt:
+                raise SyntaxError('check the jpg and txt attributes')
+            self.jpg += '/' if self.jpg[-1] != '/' else ''
+            self.txt += '/' if self.txt[-1] != '/' else ''
+            files = glob(self.jpg + '*.jpg')
+            if not files:
+                raise EnvironmentError(self.jpg + ' has no .jpg files')
+            self.info = {}
+            for file in files:
+                name = re.split(r'/|\\', file)[-1]
+                name = name.split('.')[0] + '.txt'
+                name = self.txt + name
+                if not os.path.exists(name):
+                    raise EnvironmentError(name + ' does not exists')
+                x = {}
+                with open(name, mode='r', encoding='utf8') as f:
+                    for i, line in enumerate(f):
+                        args = line.split(' ')
+                        n = len(args)
+                        if n < 5:
+                            error = name + ' has one of the lines with '
+                            error += 'less than the spected values'
+                            raise EnvironmentError(error)
+                        for j, arg in enumerate(args):
+                            if j == 0:
+                                if not arg.isnumeric():
+                                    error = 'line '.format(
+                                        i) + 'column'.format(j) + ' from '
+                                    error += name + ' has to be an integer as class name'
+                                    raise EnvironmentError(error)
+                                if not x.get(arg):
+                                    x[arg] = 1
+                                else:
+                                    x[arg] += 1
                             else:
-                                x[arg] += 1
-                        else:
-                            if not arg.strip().replace('.', '', 1).isnumeric():
-                                error = 'line '.format(i) + 'column'.format(j) + ' from '
-                                error += name + '\nhas to be an float representing '
-                                error += 'one of the coordinates of the bounding box'
-                                raise EnvironmentError(error)
-            for key in x:
-                if not self.info.get(key):
-                    self.info[key] = {'max': x[key], 'n': 1, 'sigma': x[key]}
-                else:
-                    old, new = self.info[key]['max'], x[key]
-                    self.info[key]['max'] = new if new > old else old
-                    self.info[key]['n'] += 1
-                    self.info[key]['sigma'] += new
-        for key in self.info:
-            self.info[key]['mean'] = self.info[key]['sigma'] / self.info[key]['n']
+                                if not arg.strip().replace('.', '', 1).isnumeric():
+                                    error = 'line '.format(
+                                        i) + 'column'.format(j) + ' from '
+                                    error += name + '\nhas to be an float representing '
+                                    error += 'one of the coordinates of the bounding box'
+                                    raise EnvironmentError(error)
+                for key in x:
+                    if not self.info.get(key):
+                        self.info[key] = {'max': x[key], 'n': 1,
+                                          'sum': x[key], 'X': [x[key]]}
+                    else:
+                        old, new = self.info[key]['max'], x[key]
+                        self.info[key]['max'] = new if new > old else old
+                        self.info[key]['n'] += 1
+                        self.info[key]['sum'] += new
+                        self.info[key]['X'].append(new)
+            for key in self.info:
+                mu = self.info[key]['sum'] / self.info[key]['n']
+                self.info[key]['mean'] = mu
+                sigma = sum([(x - mu) ** 2 for x in self.info[key].pop('X')])
+                variance = sigma / self.info[key]['n']
+                self.info[key]['variance'] = variance
+                # tested and is the same as x ** (1/2) and np.sqrt(x)
+                self.info[key]['stddev'] = variance ** .5
+            return self.info
+
+        samples = [self.train, self.val, self.test]
+        if False in [isinstance(x, dict) for x in samples]:
+            error = 'train, val and test must be dictionaries containing the locations '
+            error += 'of the images and labels'
+        self.info = {}
+        for name_set, sample in zip(['train', 'val', 'test'], samples):
+            sample['images'] += '/' if sample['images'][-1] != '/' else ''
+            sample['labels'] += '/' if sample['labels'][-1] != '/' else ''
+            files = glob(sample['images'] + '*.jpg')
+            if not files:
+                raise EnvironmentError(sample['images'] + ' has no .jpg files')
+            self.info[name_set] = {}
+            for file in files:
+                name = re.split(r'/|\\', file)[-1]
+                name = name.split('.')[0] + '.txt'
+                name = sample['labels'] + name
+                if not os.path.exists(name):
+                    raise EnvironmentError(name + ' does not exists')
+                x = {}
+                with open(name, mode='r', encoding='utf8') as f:
+                    for i, line in enumerate(f):
+                        args = line.split(' ')
+                        n = len(args)
+                        if n < 5:
+                            error = name + ' has one of the lines with '
+                            error += 'less than the spected values'
+                            raise EnvironmentError(error)
+                        for j, arg in enumerate(args):
+                            if j == 0:
+                                if not arg.isnumeric():
+                                    error = 'line '.format(
+                                        i) + 'column'.format(j) + ' from '
+                                    error += name + ' has to be an integer as class name'
+                                    raise EnvironmentError(error)
+                                if not x.get(arg):
+                                    x[arg] = 1
+                                else:
+                                    x[arg] += 1
+                            else:
+                                if not arg.strip().replace('.', '', 1).isnumeric():
+                                    error = 'line '.format(
+                                        i) + 'column'.format(j) + ' from '
+                                    error += name + '\nhas to be an float representing '
+                                    error += 'one of the coordinates of the bounding box'
+                                    raise EnvironmentError(error)
+                for key in x:
+                    if not self.info[name_set].get(key):
+                        self.info[name_set][key] = {'max': x[key], 'n': 1,
+                                                    'sum': x[key], 'X': [x[key]]}
+                    else:
+                        old, new = self.info[name_set][key]['max'], x[key]
+                        self.info[name_set][key]['max'] = new if new > old else old
+                        self.info[name_set][key]['n'] += 1
+                        self.info[name_set][key]['sum'] += new
+                        self.info[name_set][key]['X'].append(new)
+            for key in self.info[name_set]:
+                x_bar = self.info[name_set][key]['sum'] / \
+                    self.info[name_set][key]['n']
+                self.info[name_set][key]['mean'] = x_bar
+                sigma = sum(
+                    [(x - x_bar) ** 2 for x in self.info[name_set][key].pop('X')])
+                variance = sigma / (self.info[name_set][key]['n'] - 1)
+                self.info[name_set][key]['variance'] = variance
+                # tested and is the same as x ** (1/2) and np.sqrt(x)
+                self.info[name_set][key]['stddev'] = variance ** .5
+
         return self.info
 
     def change_label(self, old: list, new: list) -> None:
@@ -168,6 +252,14 @@ class DatasetManager:
         splits the dataset into training, validation and testing sets
         into 80% 10% and 10%
         """
+        if not isinstance(path, str):
+            error = 'path must be a string representing the path\n'
+            error += 'in which you will split the dataset'
+            raise TypeError(error)
+        path = './' + path if path[0] != '.' else path
+        path += '/' if path[-1] != '/' else ''
+        if not os.path.exists(path):
+            os.makedirs(path)
         self.jpg += '/' if self.jpg[-1] != '/' else ''
         self.txt += '/' if self.txt[-1] != '/' else ''
         names = glob(self.jpg + '*')
@@ -175,31 +267,29 @@ class DatasetManager:
         n = len(names)
         train, val_test = names[:int(n * .8)], names[int(n * .8):]
         n -= int(n * .8)
-        val, test = val_test[:int(n * .5)], val_test[int(n * .5):] 
+        val, test = val_test[:int(n * .5)], val_test[int(n * .5):]
         n = len(val), len(test)
         val, test = (test, val) if n[1] > n[0] else (val, test)
         path = './' + path if path[0] != '.' else path
         path += '/' if path[-1] != '/' else ''
         for x, dataset in zip(['train', 'val', 'test'], [train, val, test]):
             x += '/' if x[-1] != '/' else ''
+            images = path + 'images/' + x
+            labels = path + 'labels/' + x
+            if not os.path.exists(path=images):
+                os.makedirs(name=images)
+            if not os.path.exists(path=labels):
+                os.makedirs(name=labels)
             for file in dataset:
-                images = path + 'images/' + x
-                labels = path + 'labels/' + x
-                if not os.path.exists(path=images):
-                    os.makedirs(name=images)
-                if not os.path.exists(path=labels):
-                    os.makedirs(name=labels)
                 name = re.split(r'/|\\', file)[-1].split('.')[0]
                 os.replace(self.txt + name + '.txt',
                            labels + name + '.txt')
                 os.replace(self.jpg + name + '.jpg',
-                           labels + name + '.jpg')
+                           images + name + '.jpg')
             setattr(self, x[:-1], {'images': images,
                                    'labels': labels})
         self.jpg = None
         self.txt = None
-
-        print(len(train), len(val), len(test))
         self.is_splitted = True
 
 if __name__ == '__main__':
@@ -213,4 +303,17 @@ if __name__ == '__main__':
     print(dataset.summary())
     dataset.change_label(old=[15, 16], new=[1, 2])
     print(dataset.summary())
-    dataset.split_data()
+    dataset.split_data(path='./apples/')
+    print(dataset.train)
+    print(dataset.val)
+    print(dataset.test)
+    labels = {}
+    for sample in dataset.summary():
+        print(sample + ':')
+        for cls in dataset.info[sample]:
+            print('class', cls + '\n', dataset.info[sample][cls])
+            if not labels.get(cls):
+                labels[cls] = dataset.info[sample][cls]['sum']
+            else:
+                labels[cls] += dataset.info[sample][cls]['sum']
+    print(labels)
