870ad3a794ca968865f198dee10dc136185f049b
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-04-25 00:52:21 -0300

    changed loading animation

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,11 +1,11 @@
 import '../style/loader.css';
 import styles from '../style';
-
+import { apple } from '../style/assets';
 
 const Loader = (props) =>
     (
         <div className='wrapper' {... props}>
-            <div className='spinner'></div>
+            <img src={apple} className='spinner' />
             <p className={styles.paragraph}>{ props.children }</p>
         </div>
     );
