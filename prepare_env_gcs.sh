1433a70988380122bf01d9b2fa05d32725f65fc7
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-04-28 00:55:01 -0300

    updated script

--- prepare_env_gcs.sh
+++ prepare_env_gcs.sh
@@ -4,3 +4,6 @@ npm install -D tailwindcss postcss autoprefixer
 npx tailwindcss init -p
 npm install --save typescript @types/node @types/react @types/react-dom @types/jest
 npm install --save express
+npm run build
+gcloud config set project cloris-385102
+gcloud app deploy
