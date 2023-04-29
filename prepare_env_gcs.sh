4e7dd7cc37f2f9dc6108ca8e2d86551d28e1858d
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-04-29 19:56:44 -0300

    fixed confin

--- prepare_env_gcs.sh
+++ prepare_env_gcs.sh
@@ -1,7 +1,5 @@
 nvm install stable
 nvm alias default stable
-npm install -D tailwindcss postcss autoprefixer
-npm install --save typescript @types/node @types/react @types/react-dom @types/jest
 npm install --save express
 npm run build
 gcloud config set project cloris-385200
