746abc7c1c4dbd1095f711f8fcfb7f90cad6a03d
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-05-03 18:16:37 -0300

    added comment

--- install_labelImg.sh
+++ install_labelImg.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# first run this. afterwards you would be able to run labelImg by using this:
+# python3 labelimg/labelImg.py
 git clone https://github.com/tzutalin/labelImg labelimg
 cd labelimg
 pyrcc5 -o libs/resources.py resources.qrc
